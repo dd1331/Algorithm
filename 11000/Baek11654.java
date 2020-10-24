d7ceebfea30cd58d15399ce0627496c8d21b62d9
Chalie Choi <ddd1331@gmail.com>
2020-10-24 16:47:27 +0900

    11654

---
+++ 11000/Baek11654.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Baek11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char string = sc.nextLine().charAt(0);
+        System.out.println((int)string);
+        sc.close();
+    }
+}
