3c641af2adca476d8f4446f3e6bf6e4f03388871
Chalie Choi <ddd1331@gmail.com>
2020-11-08 23:44:35 +0900

    B11022

---
+++ B11022.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B11022 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int repeat = Integer.parseInt(sc.nextLine());
+        for (int i=0; i<repeat; i++) {
+            String[] input = sc.nextLine().split(" ");
+            int result = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
+            System.out.println("Case #" + (i+1) + ": " + input[0] + " + " + input[1] + " = " + result);
+        }
+        sc.close();
+    }
+}
